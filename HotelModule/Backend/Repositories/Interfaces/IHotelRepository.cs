381e14c5c4bddb94cd669dc39aa00a31a6247f94
Sanaka Vineela <168650739+Vineela-16@users.noreply.github.com>
2026-03-24 19:07:21 +0530

    Add files via upload

---
+++ HotelModule/Backend/Repositories/Interfaces/IHotelRepository.cs
@@ -0,0 +1,14 @@
+﻿using HotelBookingWebsite.Models;
+
+namespace HotelBookingWebsite.Repositories
+{
+	public interface IHotelRepository
+	{
+		Task<IEnumerable<Hotel>> GetAllAsync();
+		Task<Hotel?> GetByIdAsync(int id);
+		Task AddAsync(Hotel hotel);
+		Task UpdateAsync(Hotel hotel);
+		Task DeleteAsync(int id);
+		Task<IEnumerable<Hotel>> SearchAsync(string? query);
+	}
+}
