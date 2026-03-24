160993207c7d0f5be8ae7a8437098095b67f3678
Sanaka Vineela <168650739+Vineela-16@users.noreply.github.com>
2026-03-24 18:56:16 +0530

    Add files via upload

---
+++ HotelModule/Services/HotelService.cs
@@ -0,0 +1,48 @@
+﻿using HotelBookingWebsite.Models;
+using HotelBookingWebsite.Repositories;   
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HotelBookingWebsite.Services
+{
+	public class HotelService : IHotelService
+	{
+		private readonly IHotelRepository _repo;
+
+		public HotelService(IHotelRepository repo)
+		{
+			_repo = repo;
+		}
+
+		public async Task<IEnumerable<Hotel>> GetAllAsync()
+		{
+			return await _repo.GetAllAsync();
+		}
+
+		public async Task<Hotel?> GetByIdAsync(int id)
+		{
+			return await _repo.GetByIdAsync(id);
+		}
+
+		public async Task AddAsync(Hotel hotel)
+		{
+			await _repo.AddAsync(hotel);
+		}
+
+		public async Task UpdateAsync(Hotel hotel)
+		{
+			await _repo.UpdateAsync(hotel);
+		}
+
+		public async Task DeleteAsync(int id)
+		{
+			await _repo.DeleteAsync(id);
+		}
+
+		// ✅ SEARCH
+		public async Task<IEnumerable<Hotel>> SearchAsync(string? query)
+		{
+			return await _repo.SearchAsync(query);
+		}
+	}
+}
---
+++ HotelModule/Services/Interfaces/IHotelService.cs
@@ -0,0 +1,15 @@
+﻿using HotelBookingWebsite.Models;
+
+namespace HotelBookingWebsite.Services
+{
+	public interface IHotelService
+	{
+		Task<IEnumerable<Hotel>> GetAllAsync();
+		Task<Hotel?> GetByIdAsync(int id);
+		Task AddAsync(Hotel hotel);
+		Task UpdateAsync(Hotel hotel);
+		Task DeleteAsync(int id);
+
+		Task<IEnumerable<Hotel>> SearchAsync(string? query);
+	}
+}
